310f78245a8d441535569c9b594eef45dae93878
Kingsley Ibe <kingsleyibe66@gmail.com>
2025-02-19 18:54:08 +0100

    updated main

--- pages/api/send-otp.js
+++ pages/api/send-otp.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
     });
 
     const mailOptions = {
-      from: process.env.EMAIL_USER,
+      from: `"Note Making" <${process.env.GMAIL_USER}>`,
       to: email,
       subject: "Your OTP for Verification",
       html: `
