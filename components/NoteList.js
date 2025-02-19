1261f2175f953eb60c535694067e3b7ee516d791
Kingsley Ibe <kingsleyibe66@gmail.com>
2025-02-19 07:07:49 +0100

    updated notelist

--- components/NoteList.js
+++ components/NoteList.js
@@ -52,7 +52,7 @@ export default function NoteList({ notes, deleteNote, updateNote, deleteLoading,
               ) : (
                 <button
                   onClick={() => startEditing(note._id, note.text)}
-                  className="bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-600 transition-all"
+                  className="bg-yellow-500 text-white ml-4 px-4 py-2 rounded-lg hover:bg-yellow-600 transition-all"
                 >
                   Edit
                 </button>
