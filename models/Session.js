148056020b41ab244bba948ba4f544b6c6c0bef1
Kingsley Ibe <kingsleyibe66@gmail.com>
2025-03-03 08:22:38 +0100

    updated main

--- models/Session.js
+++ models/Session.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import User from './User'
 
 const SessionSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
